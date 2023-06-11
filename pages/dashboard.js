12571a61c5f3f50f19543f77ae531ebd58893efd
achrysaetos <achrysaetos@yahoo.com>
2023-06-11 16:59:28 -0500

    add functionality from customgpt

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { supabase } from "../utils/supabase";
 import { useUser } from "../context/user";
 import Spinner from "../components/spinner";
+
+import { useRef, useState, useEffect } from 'react';
+import Layout from '../components/layout';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
+import LoadingDots from '../components/loadingdots';
 
 export default function Dashboard({ lessons }) {
   const { user, isLoading } = useUser();
@@ -15,39 +21,225 @@ export default function Dashboard({ lessons }) {
     if (!user) router.push("/");
   }, [user]);
 
-  // Use the pre-rendered props from getStaticProps() to display all lessons
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [messageState, setMessageState] = useState({
+    messages: [
+      {
+        message: 'Hi, what would you like to learn about this document?',
+        type: 'apiMessage',
+      },
+    ],
+    history: [],
+  });
+
+  const { messages, history } = messageState;
+
+  const messageListRef = useRef(null);
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    textAreaRef.current?.focus();
+  }, []);
+
+  //handle form submission
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setError(null);
+
+    if (!query) {
+      alert('Please input a question');
+      return;
+    }
+
+    const question = query.trim();
+
+    setMessageState((state) => ({
+      ...state,
+      messages: [
+        ...state.messages,
+        {
+          type: 'userMessage',
+          message: question,
+        },
+      ],
+    }));
+
+    setLoading(true);
+    setQuery('');
+
+    try {
+      const response = await fetch('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          question,
+          history,
+        }),
+      });
+      const data = await response.json();
+      console.log('data', data);
+
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setMessageState((state) => ({
+          ...state,
+          messages: [
+            ...state.messages,
+            {
+              type: 'apiMessage',
+              message: data.text,
+              sourceDocs: data.sourceDocuments,
+            },
+          ],
+          history: [...state.history, [question, data.text]],
+        }));
+      }
+      console.log('messageState', messageState);
+
+      setLoading(false);
+
+      //scroll to bottom
+      messageListRef.current?.scrollTo(0, messageListRef.current.scrollHeight);
+    } catch (error) {
+      setLoading(false);
+      setError('An error occurred while fetching the data. Please try again.');
+      console.log('error', error);
+    }
+  }
+
+  //prevent empty submissions
+  const handleEnter = (e) => {
+    if (e.key === 'Enter' && query) {
+      handleSubmit(e);
+    } else if (e.key == 'Enter') {
+      e.preventDefault();
+    }
+  };
+
   return isLoading ? (
     <Spinner />
   ) : (
-    <div className="w-full max-w-3xl mx-auto my-24 px-2">
-      {lessons.map((lesson) => (
-        <Link key={lesson.id} href={`/${lesson.id}`}>
-          <a className="relative flex items-start justify-between rounded-xl border border-gray-100 mb-4 p-4 shadow-xl sm:p-6 lg:p-8">
-            <div className="pt-4 text-gray-500">
-              <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
-                {lesson.title}
-              </h3>
-              <p className="mt-2 hidden text-sm sm:block">
-                {lesson.description}
-              </p>
+    <>
+      <Layout>
+        <div className="mx-auto flex flex-col gap-4">
+          <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
+            Chat With Your Docs
+          </h1>
+          <main className={styles.main}>
+            <div className={styles.cloud}>
+              <div ref={messageListRef} className={styles.messagelist}>
+                {messages.map((message, index) => {
+                  let icon;
+                  let className;
+                  if (message.type === 'apiMessage') {
+                    icon = (
+                      <Image
+                        key={index}
+                        src="/bot-image.png"
+                        alt="AI"
+                        width="40"
+                        height="40"
+                        className={styles.boticon}
+                        priority
+                      />
+                    );
+                    className = styles.apimessage;
+                  } else {
+                    icon = (
+                      <Image
+                        key={index}
+                        src="/usericon.png"
+                        alt="Me"
+                        width="30"
+                        height="30"
+                        className={styles.usericon}
+                        priority
+                      />
+                    );
+                    // The latest message sent by the user will be animated while waiting for a response
+                    className =
+                      loading && index === messages.length - 1
+                        ? styles.usermessagewaiting
+                        : styles.usermessage;
+                  }
+                  return (
+                    <>
+                      <div key={`chatMessage-${index}`} className={className}>
+                        {icon}
+                        <div className={styles.markdownanswer}>
+                          <ReactMarkdown linkTarget="_blank">
+                            {message.message}
+                          </ReactMarkdown>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })}
+              </div>
             </div>
-            <span className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600">
-              New
-            </span>
+            <div className={styles.center}>
+              <div className={styles.cloudform}>
+                <form onSubmit={handleSubmit}>
+                  <textarea
+                    disabled={loading}
+                    onKeyDown={handleEnter}
+                    ref={textAreaRef}
+                    autoFocus={false}
+                    rows={1}
+                    maxLength={512}
+                    id="userInput"
+                    name="userInput"
+                    placeholder={
+                      loading
+                        ? 'Waiting for response...'
+                        : 'What is this legal case about?'
+                    }
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className={styles.textarea}
+                  />
+                  <button
+                    type="submit"
+                    disabled={loading}
+                    className={styles.generatebutton}
+                  >
+                    {loading ? (
+                      <div className={styles.loadingwheel}>
+                        <LoadingDots color="#000" />
+                      </div>
+                    ) : (
+                      // Send icon SVG in input field
+                      <svg
+                        viewBox="0 0 20 20"
+                        className={styles.svgicon}
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                      </svg>
+                    )}
+                  </button>
+                </form>
+              </div>
+            </div>
+            {error && (
+              <div className="border border-red-400 rounded-md p-4">
+                <p className="text-red-500">{error}</p>
+              </div>
+            )}
+          </main>
+        </div>
+        <footer className="m-auto p-4">
+          <a href="https://twitter.com/mayowaoshin">
+            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
           </a>
-        </Link>
-      ))}
-    </div>
+        </footer>
+      </Layout>
+    </>
   );
 }
-
-// Get all props from the lesson table at build time
-export const getStaticProps = async () => {
-  const { data: lessons } = await supabase.from("lesson").select("*");
-
-  return {
-    props: {
-      lessons,
-    },
-  };
-};
